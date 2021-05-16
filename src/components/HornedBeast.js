47ee31659524f831d0d78a646841252d48faa10d
Abdelrahman Mohammad Al Daour <Daour.995@outlook.com>
2021-05-16 20:34:26 +0300

    Finished Lab01

---
+++ src/components/HornedBeast.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class HornedBeast extends React.Component{
+
+    render(){
+        return(
+            <div>
+                <h2>{this.props.title}</h2>
+                <img src={this.props.imgsrc} alt={this.props.title}/>
+                <p >{this.props.description} </p>
+            </div>
+        )
+    }
+
+}
+
+export default HornedBeast;
