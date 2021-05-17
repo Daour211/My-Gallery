c51de0c51d2bdbafaf4ee02606f4f3a5a508a3da
Abdelrahman Mohammad Al Daour <Daour.995@outlook.com>
2021-05-17 15:29:54 +0300

    finished lab02

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import HornedBeast from './HornedBeast'
+import hornsData from './data.json'
+
+
 class Main extends React.Component {
+
+
 
     render() {
         return (
-            <div>
-                <HornedBeast title={'UniWhal'}
+         <>
+                {hornsData.map(item => {
+
+                    return (
+                        <HornedBeast
+                            title={item.title}
+                            imgsrc={item.image_url}
+                            description={item.description}
+
+                        />
+
+                    )
+                })}
+
+
+            </>
+        )
+    }
+
+}
+
+export default Main;
+
+{/* <HornedBeast title={'UniWhal'}
                     imgsrc={'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg'}
                     description={'A unicorn and a narwhal nuzzling their horns'} />
 
@@ -15,14 +42,4 @@ class Main extends React.Component {
 
                 <HornedBeast title={'Unicorn Head'}
                     imgsrc={'https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg'}
-                    description={'Someone wearing a creepy unicorn head mask'} />
-
-
-
-            </div>
-        )
-    }
-
-}
-
-export default Main;
+                    description={'Someone wearing a creepy unicorn head mask'} /> */}
